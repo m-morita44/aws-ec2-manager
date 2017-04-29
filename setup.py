1d55aa367a626629f07df724b5db15fd19043d37
Masato Morita <m.morita44@hotmail.com>
2017-04-30 05:55:03 +0800

    Add install_requires at setup.

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
-#!/usr/bin/env python3.5
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 try:
-    from setuptools import setup
+    from setuptools import setup, find_packages
 except ImportError:
     from distutils.core import setup
 
 setup(name='aws-ec2-manager',
-      version='1.5',
-      description='AWS EC2 Manager',
+      version='1.5.1',
+      description='the tools is managing AWS EC2 instance by python3.',
+      url='https://github.com/m-morita44',
       author='Masato Morita',
       author_email='m.morita44@hotmail.com',
-      url='https://github.com/m-morita44',
+      license='MIT',
       scripts=['aws-ec2-manager.py'],
-      packages=["aws", "command"],
-      test_suite="test",
-      )
+      packages=find_packages(),
+      install_requires=['boto3'],
+      test_suite='tests')
